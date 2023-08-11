e3fcec4a42a06c997106a4e782448f9155a88838
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-08-10 18:58:46 -0700

    TASK 4:A C program that prints out a text using the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/*Outputs a string followed by a new line.*/
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
