08be1d5957c1ef05949222a8f60e6182e8ef47c9
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-08-11 18:43:31 -0700

    Corrections to TASK 0 on betty style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,20 +3,23 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n>0) {
+	/*If...else statement to check the variable n*/
+	if (n > 0)
 		printf("%d is positive\n", n);
-	} else if (n<0) {
+	else if (n < 0)
 		printf("%d is negative\n", n);
-	} else {
+	else
 		printf("%d is zero\n", n);
-	}
 	return (0);
 }
