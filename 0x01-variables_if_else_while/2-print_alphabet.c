5e5991a7a6ef9bc73535e5874a93dea162a552c2
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-08-11 19:54:07 -0700

    TASK 2:A C program that prints the alphabet in lowercase, followed by a new line using only the putchar function and only twice

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	/*The decimal value for lowercase 'a' is 97 and that of 'z' is 123*/
+	for (a = 97; a < 123; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
