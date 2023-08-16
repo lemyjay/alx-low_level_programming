18613d439c909d51e401baa17d8eae5d9f099aed
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-08-16 22:40:20 +0000

    code for TASK 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - program that prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line
+ * Return: Always 0.
+*/
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, fibsum;
+	float totalSum;
+
+	while (true)
+	{
+		fibsum = fib1 + fib2;
+		if (fibsum > 4000000)
+			break;
+		if ((fibsum % 2) == 0)
+			totalSum += fibsum;
+
+		fib1 = fib2;
+		fib2 = fibsum;
+	}
+	printf("%.0f\n", totalSum);
+
+	return (0);
+}
