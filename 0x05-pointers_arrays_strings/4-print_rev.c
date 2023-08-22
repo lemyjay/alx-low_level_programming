05df2acdd05358fe3757bb0501a9cc3f359cc2ef
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-08-22 23:52:07 +0000

    TASK 5:A function that reverses a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -17,14 +17,6 @@ void print_rev(char *s)
 	while (s[length] != '\0')
 		length++;
 
-
-	/*
-	 * After calculating the length of the string,
-	 * you have moved the s pointer to the end of the string using s++.
-	 * so now the pointer is pointing to the last character of the string s
-	 * we then start printing from there towards the first character
-	 * by decrementing
-	 */
 	for (i = length - 1; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * rev_string - A function that reverses a string
+ *
+ * @s: a pointer that holds the address of the string
+ * Return: returns an integer.
+ */
+
+void rev_string(char *s)
+{
+	int length = 0;
+	int i;
+	int a = 0;
+	char temp;
+
+	while (s[length] != '\0')
+		length++;
+
+	for (i = length - 1; (i >= 0) && (i != a); i--, a++)
+	{
+		temp = s[a];
+		s[a] = s[i];
+		s[i] = temp;
+	}
+	s[length] = '\0';
+}
