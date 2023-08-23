635e0cae9aa4d46811353e1ecfc53017774b559a
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-08-23 01:33:52 +0000

    correction to task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,12 +15,15 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n >= 1)
 	{
-		printf("%d", *(a + i));
-		if (i != n - 1)
-			printf(", ");
-		else
-			printf("\n");
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", *(a + i));
+			if (i != n - 1)
+				printf(", ");
+			else
+				printf("\n");
+		}
 	}
 }
