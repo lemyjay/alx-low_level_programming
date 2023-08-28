73fdd8e7fc0a66a1cd72d68a439eaf442e806adf
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-08-28 02:42:23 +0000

    corrections to task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -17,19 +17,22 @@
 char *leet(char *s)
 {
 	int i = 0;
+	char *leet_chars = "AEOTL";
+	char *leet_replacements = "43071";
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		int j = 0;
+
+		while (leet_chars[j] != '\0')
+		{
+			if (s[i] == leet_chars[j] || s[i] == leet_chars[j] + 32)
+			{
+				s[i] = leet_replacements[j];
+				break;
+			}
+			j++;
+		}
 		i++;
 	}
 
