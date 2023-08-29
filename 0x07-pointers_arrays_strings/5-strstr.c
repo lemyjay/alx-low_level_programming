d763205da7aa1b2673f106fdf1b9720dfdc2ee0d
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-08-29 23:49:13 +0000

    correction to task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,8 +9,7 @@
  * in the string haystack. The terminating null bytes (\0) are not compared
  *
  * @haystack: a pointer that holds the address to the string
- * on which the operation
- * is to be performed
+ * on which the operation is to be performed
  * @needle: a pointer that holds the address to the character to search
  * Return: a pointer to the beginning of the located substring, or NULL
  * if the substring is not found.
@@ -18,19 +17,24 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	unsigned int i = 0, j = 0;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	while (haystack[i])
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		while (needle[j] && (haystack[i] == needle[0]))
 		{
-			if (haystack[i + j] != needle[j])
-				break;/* Exit inner loop if mismatch is found */
-
-			if (needle[j + 1] == '\0')
-				return (&haystack[i]);/* Return the pointer to the located substring */
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
 		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
 	}
-
-	return (NULL);/* Substring not found */
+	return (0);
 }
