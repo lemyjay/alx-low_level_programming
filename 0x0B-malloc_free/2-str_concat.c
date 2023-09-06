ca26e07d4614ad0dc0f47bc4e3c1f3363e7a5bbe
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-09-06 02:59:22 +0000

    TASK 2 - A function that concatenates two strings.
    Prototype: char *str_concat(char *s1, char *s2);
    The returned pointer should point to a newly allocated space in memory which contains the contents of s1, followed by the contents of s2, and null terminated
    if NULL is passed, treat it as an empty string
    The function should return NULL on failure.

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_length - A function that returns the length of a string as an integer.
+ *
+ * @s: a pointer that holds the address to the string.
+ * Return: the length of the string as an integer
+ */
+
+unsigned int str_length(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+
+	return (length);
+}
+
+/**
+ * str_concat - A function that concatenates two strings. The returned pointer
+ * points to a newly allocated space in memory which contains the contents of
+ * s1, followed by the contents of s2, and null terminated. If NULL is passed,
+ * it is treated as an empty string.
+ *
+ * @s1: a pointer that holds the address of the first string on which the
+ * operation is to performed.
+ * @s2: a pointer that holds the address of the second string.
+ * Return: NULL on failure (insufficient memory was available) || a
+ *  pointer to the newly allocated space in memory which contains the
+ *  concatenation.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *new_str;
+	unsigned int i, j, length1, length2;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	length1 = str_length(s1);
+	length2 = str_length(s2);
+
+	new_str = (char *)malloc((length1 + length2 + 1) * sizeof(char));
+
+	if (new_str == NULL)
+		return (NULL);
+
+	for (i = 0; i < length1; i++)
+		new_str[i] = s1[i];
+
+	for (j = 0; i < length2; j++, i++)
+		new_str[i] = s2[j];
+
+	new_str[i] = '\0';
+
+	return (new_str);
+}
