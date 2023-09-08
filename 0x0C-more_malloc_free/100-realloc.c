088868e409022c15dc592ca4ebcaa4dd3860de06
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-09-08 03:46:48 +0000

    TASK 4 - A function that reallocates a memory block using malloc and free
    Prototype: void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
    where ptr is a pointer to the memory previously allocated with a call to malloc: malloc(old_size)
    old_size is the size, in bytes, of the allocated space for ptr
    and new_size is the new size, in bytes of the new memory block
    The contents will be copied to the newly allocated space, in the range from the start of ptr up to the minimum of the old and new sizes
    If new_size > old_size, the “added” memory should not be initialized
    If new_size == old_size do not do anything and return ptr
    If ptr is NULL, then the call is equivalent to malloc(new_size), for all values of old_size and new_size
    If new_size is equal to zero, and ptr is not NULL, then the call is equivalent to free(ptr). Return NULL
    Don’t forget to free ptr when it makes sense
    FYI: The standard library provides a different function: realloc.

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - A function that reallocates a memory block using malloc and free.
+ *
+ * @ptr: a pointer that points to the memory previously allocated.
+ * @old_size: the old size in bytes of the allocated space for ptr.
+ * @new_size: the new size in bytes to the new memory block.
+ *
+ * Return: If success, a pointer to the reallocated memory || If failure or
+ * new_size is 0, return NULL.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new;
+	unsigned int i, copy_size;
+	char *src;
+	char *dest;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (new_size == old_size)
+		return (ptr);
+
+	new = malloc(new_size);
+	if (new == NULL)
+		return (NULL);
+
+	if (old_size < new_size)
+		copy_size = old_size;
+	else
+		copy_size = new_size;
+	src = (char *)ptr;
+	dest = (char *)new;
+	for (i = 0; i < copy_size; i++)
+		dest[i] = src[i];
+	free(ptr);
+
+	return (new);
+}
