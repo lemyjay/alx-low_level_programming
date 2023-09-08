cf6358a312397a2d9bbcc5002c873979896fc35a
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-09-08 02:52:48 +0000

    correction to task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (new == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		new[i] = 0;
 
 	return ((void *)new);
