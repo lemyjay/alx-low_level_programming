97699845a8cad0ee53da4ba8fc8d7090515ce2af
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-09-08 01:54:01 +0000

    correction to task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -55,7 +55,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; i < length1; i++)
 		new_str[i] = s1[i];
 
-	for (j = 0; i < length2; j++, i++)
+	for (j = 0; j < length2; j++, i++)
 		new_str[i] = s2[j];
 
 	new_str[i] = '\0';
