e6fd75d44ce59830442455e0b58d402b500345bd
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-09-08 04:56:20 +0000

    correction to task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,7 +21,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
@@ -46,5 +46,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		dest[i] = src[i];
 	free(ptr);
 
-	return ((void *)dest);
+	return (new);
 }
