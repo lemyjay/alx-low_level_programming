e2b2a42584dc0535c5aba52affe9a937fe10fbc5
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-09-12 02:29:02 +0000

    TASK 0 - A definition fo a new type struct dog with the following elements:name, type = char * age, type = float owner, type = char *

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - A struct for the/an object dog.
+ * @name: The name of the doge.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: Just creating a struct for the object dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
