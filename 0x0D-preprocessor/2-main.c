04b5c5ba9bb65b01f45a5b932735b1225d28cc81
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-09-12 00:36:29 +0000

    TASK 2 - A program that prints the name of the file it was compiled from, followed by a new line. Use of standard libraries are allowed.

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point to the program
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
