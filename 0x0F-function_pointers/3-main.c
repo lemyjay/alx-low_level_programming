37e8f09fb2f43756d38f04e76df96184efa9eabf
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-09-14 04:51:24 +0000

    corrections to task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -27,12 +27,6 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 
 	return (0);
