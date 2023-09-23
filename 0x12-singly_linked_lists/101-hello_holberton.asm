44eb452eaccee4824eb3c7a32893e45b3eb1bfd2
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-09-23 02:38:34 +0000

    correction

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,15 @@
 section .data
-hello_msg db 'Hello, Holberton',0xA,0
+    hello db "Hello, Holberton",10,0  ; Define the string with a newline character and null terminator
 
 section .text
+    global main
+
 extern printf
-global main
 
 main:
-push rbp
-mov rdi, hello_msg
-call printf
-pop rbp
-ret
+    push rbp           ; Save the base pointer
+    mov rdi, hello     ; Load the address of the string into rdi (1st argument for printf)
+    call printf        ; Call the printf function to print the string
+    pop rbp            ; Restore the base pointer
+    ret               ; Return from the main function
+
