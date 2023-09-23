b59f470b0015e31c90da625bd1f6273d343e947a
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-09-23 02:43:15 +0000

    correction

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,16 @@
 section .data
-    hello db "Hello, Holberton",10,0  ; Define the string with a newline character and null terminator
+    format db "Hello, Holberton",10,0   ; The format string with newline and null terminator
+    fmt_len equ $ - format              ; Calculate the length of the format string
 
 section .text
     global main
 
-extern printf
+    extern printf
 
 main:
-    push rbp           ; Save the base pointer
-    mov rdi, hello     ; Load the address of the string into rdi (1st argument for printf)
-    call printf        ; Call the printf function to print the string
-    pop rbp            ; Restore the base pointer
-    ret               ; Return from the main function
+    push rbp                            ; Preserve the base pointer
+    mov rdi, format                    ; Load the address of the format string into rdi
+    call printf                        ; Call the printf function
+    pop rbp                             ; Restore the base pointer
+    ret                                ; Return from the main function
 
