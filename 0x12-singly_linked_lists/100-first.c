d9a750fb1d916513b8391669022c7d322cff293e
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-09-23 01:43:02 +0000

    Task 100 - A function that prints something before the main function is executed.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * startup_function - Function to be executed before main.
+ *
+ * Description: This function will be executed automatically before the main
+ * function is called.
+ */
+void startup_function(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
+}
