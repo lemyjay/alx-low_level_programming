aa1c9fcb459b378957ac7cfd38f94c200d904275
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-09-26 15:26:36 +0000

    Task 10 - A function that deletes the node at index index of a listint_t linked list.
    Prototype: int delete_nodeint_at_index(listint_t **head, unsigned int index);
    where index is the index of the node that should be deleted. Index starts at 0
    Returns: 1 if it succeeded, -1 if it failed.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int count;
 	listint_t *insert_new_node, *temp;
 
-	if (*head == NULL || head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	insert_new_node = malloc(sizeof(listint_t));
