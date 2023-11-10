3f1fa6aea045ecb5f976ad57426331bf086a56ff
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-11-10 09:34:11 +0000

    the readme file for now

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - A function that prints all the elements in a doubly linked
+ * list dlistint_t
+ *
+ * @h: the pointer to the head of the list
+ *
+ * Return: the number of nodes.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int a;
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,3 @@
+# 0x17. C - Doubly linked lists
+
+This directory is about files on Doubly linked lists in C
