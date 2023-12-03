f29445b04a55967815396342fa8463ad3b8d0ee8
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-03 02:53:24 +0000

    the file to task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - A function that creates a hash table
+ * 
+ * @size: the size of the array(table)
+ * 
+ * Return: a pointer to a newly created hash table OR NULL if it fails
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t new;
+    
+    new->array = malloc(sizeof(hash_node_t) * size);
+
+    return (new):
+}
