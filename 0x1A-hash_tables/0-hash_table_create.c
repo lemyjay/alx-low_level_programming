9fc3d2e05dcd0ca934a235abc60dcab6aa0941fa
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-03 03:21:09 +0000

    file work for task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,26 +2,26 @@
 
 /**
  * hash_table_create - A function that creates a hash table
- * 
+ *
  * @size: the size of the array(table)
- * 
+ *
  * Return: a pointer to a newly created hash table OR NULL if it fails
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new = malloc(sizeof(hash_table_t));
+	hash_table_t *new = malloc(sizeof(hash_table_t));
 
-    if (new != NULL)
-    {
-        new->array = malloc(sizeof(hash_node_t *) * size);
-        new->size = size;
+	if (new != NULL)
+	{
+		new->array = malloc(sizeof(hash_node_t *) * size);
+		new->size = size;
 
-        if (new->array == NULL)
-        {
-            free(new);
-            return (NULL);
-        }
-    }
+		if (new->array == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+	}
 
-    return (new);
+	return (new);
 }
