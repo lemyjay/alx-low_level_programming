c08bb5e6c02b016726020fa9688becd6227572d2
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-03 02:54:22 +0000

    some additional content

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
     hash_table_t new;
     
     new->array = malloc(sizeof(hash_node_t) * size);
+    new->size = size
 
     return (new):
 }
