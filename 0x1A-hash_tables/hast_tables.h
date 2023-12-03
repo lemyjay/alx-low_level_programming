ab3f2c62e3a582ddfd3d83210a27e69d54462cdc
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-03 02:29:18 +0000

    trying to create the header file for this project

---
+++ 0x1A-hash_tables/hast_tables.h
@@ -0,0 +1,12 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+/* C standard library included for this project */
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Function prototypes */
+
+
+
+#endif /* HASH_TABLES_H */
