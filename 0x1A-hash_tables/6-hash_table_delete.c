db214f8d6778e65f0e839ad05181cd71a0c99d64
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-04 15:58:09 +0000

    corrections

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -22,7 +22,7 @@ void hash_table_delete(hash_table_t *ht)
             {
                 current = current->next;
                 free(temp1->key);
-                free(temp1->value;);
+                free(temp1->value);
                 free(temp1);
 
             }
