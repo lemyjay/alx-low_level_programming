51748b96d923536fa38b933370a64e5d57dbc3c7
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-04 08:46:33 +0000

    correction to the file

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,7 +8,7 @@
  * 
  * Return: The index at which the key/value pair should be stored in the array of the hash table
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
     unsigned long int hash = hash_djb2(key);
 
