db677d3ef6de9764adedee0addcb1682ce60ca26
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-04 16:45:35 +0000

    betty coding style

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -2,34 +2,31 @@
 
 /**
  * hash_table_delete - A function that deletes a hash table.
- * 
+ *
  * @ht: The hash table
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    if (ht != NULL)
-    {
-        unsigned long int size = ht->size, i;
+	if (ht != NULL)
+	{
+		unsigned long int size = ht->size, i;
+		
+		for (i = 0; i < size; i++)
+		{
+			hash_node_t *current = ht->array[i];
 
-        for (i = 0; i < size; i++)
-        {
-            hash_node_t *current = ht->array[i];
+			while (current != NULL)
+			{
+				hash_node_t *temp = current->next;
+				free(current->key);
+				free(current->value);
+				free(current);
+				
+				current = temp;
+			}
+		}
 
-            while (current != NULL)
-            {
-                hash_node_t *temp = current->next;
-
-                free(current->key);
-                free(current->value);
-                free(current);
-                
-                current = temp;
-
-            }
-        }
-
-        free(ht->array);
-        free(ht);
-
-    }
+		free(ht->array);
+		free(ht);
+	}
 }
