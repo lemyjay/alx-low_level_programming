bb867bd2e57003f81b1fa4562f43fb5e4d849390
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-04 15:54:36 +0000

    the files to task 6

---
+++ 6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - A function that deletes a hash table.
+ * 
+ * @ht: The hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+    if (ht != NULL)
+    {
+        hash_node_t *current = ht->array[0];
+        unsigned long int size = ht->size, i;
+
+        free(ht->array);
+        free(ht);
+        for (i = 0; i < size; i++)
+        {
+            hash_node_t *temp = current[i + 1], *temp1 = current;
+
+            while (current != NULL)
+            {
+                current = current->next;
+                free(temp1->key);
+                free(temp1->value;);
+                free(temp1);
+
+            }
+            current = temp;
+        }
+
+    }
+}
