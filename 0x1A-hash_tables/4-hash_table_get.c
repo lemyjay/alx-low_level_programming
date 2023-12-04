549c2569cd7c0aa1c965ce215d7842660a202b30
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-04 12:19:37 +0000

    correction to the file to task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
     unsigned long int index;
     hash_node_t *current;
 
-    if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+    if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
     index = key_index((unsigned char *)key, ht->size);
