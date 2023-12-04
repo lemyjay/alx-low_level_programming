b6733a495b1e78d6e081a4c305de6b9a51709104
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-04 16:49:10 +0000

    betty coding style

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,7 +10,7 @@ void hash_table_delete(hash_table_t *ht)
 	if (ht != NULL)
 	{
 		unsigned long int size = ht->size, i;
-		
+
 		for (i = 0; i < size; i++)
 		{
 			hash_node_t *current = ht->array[i];
@@ -18,10 +18,11 @@ void hash_table_delete(hash_table_t *ht)
 			while (current != NULL)
 			{
 				hash_node_t *temp = current->next;
+
 				free(current->key);
 				free(current->value);
 				free(current);
-				
+
 				current = temp;
 			}
 		}
