89242be003ff43279d4f26c6c44fff66945681f9
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-04 12:55:50 +0000

    correction to task 5's file

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -20,11 +20,11 @@ void hash_table_print(const hash_table_t *ht)
             current = ht->array[i];
             if (current != NULL)
             {
-                str = fprintf(str, "'%s' : '%s'", current->key, current->value);
+                str = sprintf(str, "'%s' : '%s'", current->key, current->value);
                 current = current->next;
                 while (current != NULL)
                 {
-                    str = fprintf(str, "'%s' : '%s'", current->key, current->value);
+                    str = sprintf(str, "'%s' : '%s'", current->key, current->value);
                     current = current->next;
                 }
             }
