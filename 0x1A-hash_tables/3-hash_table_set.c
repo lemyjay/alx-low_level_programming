7847d48e1b858593555cf34736118e3b6643245d
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-04 16:32:17 +0000

    corrections

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -48,7 +48,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-	new_node->next = ht->array[index];
+	new_node->next = NULL /*ht->array[index]*/;
 	ht->array[index] = new_node;
 
 	return (1);
