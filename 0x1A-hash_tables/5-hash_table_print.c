361ad46aea210f3440b72c3a541d219c441035e1
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-04 12:50:51 +0000

    Task 5's files

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - A function that prints a hash table
+ * 
+ * @ht: The hash table 
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+    if (ht != NULL)
+    {
+        hash_node_t *current;
+        char *str = "";
+        unsigned long int i, arr_size = ht->size;
+        
+        if (ht->array == NULL)
+            return;
+        for (i = 0; i < arr_size; i++)
+        {
+            current = ht->array[i];
+            if current != NULL
+            {
+                str = fprint(str, "'%s' : '%s'", current->key, current->value);
+                current = current->next;
+                while (current != NULL)
+                {
+                    str = fprint(str, "'%s' : '%s'", current->key, current->value);
+                    current = current->next;
+                }
+            }
+
+                
+        }
+
+    }
+}
