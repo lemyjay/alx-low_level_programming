13e122059ece6aaad1c40360425443381de1f2ac
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-04 11:55:47 +0000

    correction to the file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,38 +12,46 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t **current = ht->array;
-    hash_node_t *temp = NULL;
+     unsigned long int index;
+    hash_node_t *new_node, *current;
 
-    index = key_index((const unsigned char *)key, 1024/*((sizeof(ht->array)) / (sizeof(hash_node_t *)))*/);
-    if (current[index] == NULL)
+    if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+        return (0);
+
+    index = key_index((unsigned char *)key, ht->size);
+
+    current = ht->array[index];
+    
+    while (current != NULL)
     {
-        hash_node_t *node = malloc(sizeof(hash_node_t));
-
-        if (node == NULL)
-            return (0);
-        node->key = (char *)key;
-        node->value = strdup(value);
-        
-        current[index] = node;
+        if (strcmp(current->key, key) == 0)
+        {
+            free(current->value);
+            current->value = strdup(value);
+            if (current->value == NULL)
+                return (0);
+            return (1);
+        }
+        current = current->next;
     }
-    else
+
+    /* Key doesn't exist, create a new node */
+    new_node = malloc(sizeof(hash_node_t));
+    if (new_node == NULL)
+        return (0);
+
+    new_node->key = strdup(key);
+    new_node->value = strdup(value);
+    if (new_node->key == NULL || new_node->value == NULL)
     {
-        hash_node_t *node = malloc(sizeof(hash_node_t));
-
-        if (node == NULL)
-            return (0);
-        node->key = (char *)key;
-        node->value = strdup(value);
-
-        temp = current[index]->next;
-        while (temp != NULL)
-            temp = temp->next;
-        
-        temp = node;
-
+        free(new_node->key);
+        free(new_node->value);
+        free(new_node);
+        return (0);
     }
+
+    new_node->next = ht->array[index];
+    ht->array[index] = new_node;
 
     return (1);
 }
