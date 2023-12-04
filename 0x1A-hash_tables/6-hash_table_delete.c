9fc35f23e00869d471c1511065cf6737877abe26
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-04 16:08:58 +0000

    corrections

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,25 +9,26 @@ void hash_table_delete(hash_table_t *ht)
 {
     if (ht != NULL)
     {
-        hash_node_t *current = ht->array[0];
         unsigned long int size = ht->size, i;
 
-        free(ht->array);
-        free(ht);
         for (i = 0; i < size; i++)
         {
-            hash_node_t *temp = &(current[i + 1]), *temp1 = current;
+            hash_node_t *current = ht->array[1];
 
             while (current != NULL)
             {
-                current = current->next;
-                free(temp1->key);
-                free(temp1->value);
-                free(temp1);
+                hash_node_t *temp = current->next;
+
+                free(current->key);
+                free(current->value);
+                
+                current = temp;
 
             }
-            current = temp;
         }
+
+        free(ht->array);
+        free(ht);
 
     }
 }
