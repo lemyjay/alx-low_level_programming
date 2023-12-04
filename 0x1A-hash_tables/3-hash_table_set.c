6eb00984ebe4852d2c2d1f02aa0cf6e8e39329de
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-04 10:48:16 +0000

    the file to task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,47 @@
+#include "hash_tables.h"
+
+/**
+ * hast_table_set - A function that adds an element to the hash table
+ * 
+ * @ht: The hash table you want to add or update the key/value to 
+ * @key: The key, which cannot be an empty string 
+ * @value: The value associated with the key. It is to be duplicated and
+ * can be an empty string.
+ * 
+ * Return: 1 if it succeeded, 0 otherwise 
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    unsigned long int index = key_index(key), i = 0;
+    hash_table_t *current = ht->array;
+
+    if (current[index] == NULL)
+    {
+        hash_node_t *node = malloc(sizeof(hash_node_t));
+
+        if (node == NULL)
+            return (0);
+        node->key = key;
+        node->value = strdup(value);
+        
+        current[index] = node;
+    }
+    else
+    {
+        hash_node_t *node = malloc(sizeof(hash_node_t));
+
+        if (node == NULL)
+            return (0);
+        node->key = key;
+        node->value = strdup(value);
+
+        current = (current[index])->next;
+        while (current != NULL)
+            current = (current[index])->next;
+        
+        current = node;
+
+    }
+
+    return (1)
+}
