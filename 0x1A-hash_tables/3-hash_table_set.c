4eca5e972d704daf49350ee03bdfc17eed4b8a4e
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-04 16:33:33 +0000

    some changes

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node, *current;
+	hash_node_t *new_node = NULL, *current = NULL;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
