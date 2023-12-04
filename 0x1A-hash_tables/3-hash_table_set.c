9ad957add81f0cd818868169dda3edf4e1e25f57
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-04 11:02:53 +0000

    correction to the file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
     unsigned long int index;
-    hash_node_t *current = ht->array;
+    hash_node_t **current = ht->array;
 
     index = key_index((const unsigned char *)key, ((sizeof(current)) / (sizeof(hash_node_t))));
     if (current[index] == NULL)
