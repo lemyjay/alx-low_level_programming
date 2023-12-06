1b147b61b47cba4f159604684c08ba779f7c0abc
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-06 18:14:01 +0000

    task 0 errors'

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,9 +9,13 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new = malloc(sizeof(hash_table_t));
+	hash_table_t *new;
 	unsigned long int i;
 
+	if (size == 0)
+		return (NULL);
+
+	new = malloc(sizeof(hash_table_t));
 	if (new != NULL)
 	{
 		new->array = malloc(sizeof(hash_node_t *) * size);
